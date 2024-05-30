7e0ec1efa60af2b1e312a1105543f0dbeba7ee91
lujunji4113 <lujunji4113@gmail.com>
2024-05-30 07:43:28 +0000

    feat: wechat official account collection

---
+++ routes/wechat/official-account/collection.ts
@@ -0,0 +1,81 @@
+import { FreshContext } from "$fresh/server.ts";
+import { load } from "npm:cheerio@1.0.0-rc.12";
+import { Temporal } from "npm:@js-temporal/polyfill@0.4.4";
+import { ItemOptions, RSS } from "../../../utils/rss.ts";
+import { isDefined } from "../../../utils/types.ts";
+
+export const handler = async (
+  req: Request,
+  _ctx: FreshContext,
+): Promise<Response> => {
+  const urlInstance = new URL(req.url);
+
+  const albumId = urlInstance.searchParams.get("album_id");
+
+  const targetUrl =
+    `https://mp.weixin.qq.com/mp/appmsgalbum?action=getalbum&album_id=${albumId}`;
+
+  const response = await fetch(targetUrl);
+  const html = await response.text();
+
+  const $ = load(html);
+
+  const title = $(".album__label-title").text();
+
+  const items: ItemOptions[] = (await Promise.all(
+    $(".album__list")
+      .children()
+      .toArray()
+      .map(async (item) => {
+        const $item = $(item);
+
+        const link = $item.attr("data-link");
+        if (!link) {
+          return null;
+        }
+
+        const response = await fetch(link, {
+          headers: {
+            "User-Agent":
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+          },
+        });
+        const itemDetailHtml = await response.text();
+        const $itemDetail = load(itemDetailHtml);
+
+        const desc = $itemDetail('meta[name="description"]').attr("content");
+        if (!itemDetailHtml.includes("description")) {
+          console.log(itemDetailHtml);
+        }
+
+        const timestamp = $item.find(".js_article_create_time").text();
+        const pubDate = new Date(parseInt(timestamp, 10) * 1000);
+
+        const style = $item.find(".album__item-img").attr("style");
+        const [, imageUrl] = /url\((.*)\)/.exec(style ?? "") ?? [];
+
+        return {
+          guid: $item.attr("data-msgid"),
+          title: $item.attr("data-title") ?? "",
+          link,
+          pubDate: Temporal.Instant.from(
+            pubDate.toISOString(),
+          ).toLocaleString("zh-CN"),
+          description: `<img src="${imageUrl}" /><p>${desc}</p>`,
+        };
+      }),
+  )).filter(isDefined);
+
+  const rss = new RSS({
+    title,
+    link: targetUrl,
+    description: "",
+    items,
+  });
+
+  return new Response(rss.toXML(), {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+    },
+  });
+};
---
+++ utils/general.ts
@@ -0,0 +1,6 @@
+export const waitForTimeout = (timeout: number) =>
+  new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, timeout);
+  });
